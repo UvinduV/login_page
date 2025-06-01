dd00ea60608d280199943bca0a7b154151a47243
UvinduV <uvindusanketh470@gmail.com>
2025-06-01 18:37:26 +0530

    create basic home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,12 @@
 export function Home(){
-    return ("Home")
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-50">
+            <div className="max-w-md w-full space-y-8 p-8 bg-white rounded-lg shadow-md">
+                <h1 className="text-2xl font-bold text-gray-900 text-center">
+                    Welcome to Anywhere App!
+                </h1>
+
+            </div>
+        </div>
+    )
 }
