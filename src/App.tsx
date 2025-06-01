0f1e71fb2ec067e79aefc922c308b8d61da332b0
UvinduV <uvindusanketh470@gmail.com>
2025-06-01 17:32:28 +0530

    create basic routing in login and home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import './App.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {Login} from "./pages/Login.tsx";
+import {Home} from "./pages/Home.tsx";
 
 function App() {
+  const routes= createBrowserRouter([
+    {
+      path:'/', element:<Login/>
+    },
+    {
+      path:'/home', element:<Home/>
+    }
+  ])
 
   return (
     <>
-
+      <RouterProvider router={routes}></RouterProvider>
     </>
   )
 }
